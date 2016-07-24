6fe991dc53a0444a51f6475260de37cf8edf5d7b
Mr Maker <make-all@github.com>
2016-07-24 13:17:26 +0800

    Update to use HTML5 instead of tables.
    Based on changes to SourceSVN between master and master-1.3.x

--- SourcePhabricatorSVN/SourcePhabricatorSVN.php
+++ SourcePhabricatorSVN/SourcePhabricatorSVN.php
@@ -115,18 +115,27 @@ class SourcePhabricatorSVNPlugin extends SourceSVNPlugin {
 		$t_path = $this->get_phabsvn_path( $p_repo );
 
 ?>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'url' ) ?></td>
-<td><input name="phabsvn_url" maxlength="250" size="40" value="<?php echo string_attribute( $t_url ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'name' ) ?></td>
-<td><input name="phabsvn_name" maxlength="250" size="40" value="<?php echo string_attribute( $t_name ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'path' ) ?></td>
-<td><input name="phabsvn_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_path ) ?>"/></td>
-</tr>
+<div class="field-container">
+    <label><span><?php echo plugin_lang_get( 'url' ) ?></span></label>
+    <span class="input">
+        <input name="phabsvn_url" maxlength="250" size="40" value="<?php echo string_attribute( $t_url ) ?>"/>
+    </span>
+    <span class="label-style"></span>
+</div>
+<div class="field-container">
+    <label><span><?php echo plugin_lang_get( 'name' ) ?></span></label>
+    <span class="input">
+        <input name="phabsvn_name" maxlength="250" size="40" value="<?php echo string_attribute( $t_name ) ?>"/>
+    </span>
+    <span class="label-style"></span>
+</div>
+<div class="field-container">
+    <label><span><?php echo plugin_lang_get( 'path' ) ?></span></label>
+    <span class="input">
+        <input name="phabsvn_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_path ) ?>"/>
+    </span>
+    <span class="label-style"></span>
+</div>
 <?php
 
 		return parent::update_repo_form( $p_repo );
